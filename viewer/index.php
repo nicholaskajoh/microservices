dc530b228eadd9fd3dcaafb42d922e97cc969fd1
Nicholas Kajoh <kajohterna@gmail.com>
2019-01-29 06:23:52 +0100

    viewer service

---
+++ viewer/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>YOLO Online</title>
+</head>
+<body>
+    <h1>YOLO Online</h1>
+
+    <?php if (isset($_GET['input_image']) && $_GET['input_image'] == 0) echo '<em>No image uploaded!</em>'; ?>
+    <?php if (isset($_GET['new_output']) && $_GET['new_output'] == 1) echo '<em>Image processed successfully!</em>'; ?>
+    <?php if (isset($_GET['processed']) && $_GET['processed'] == 0) echo '<em>Image processing failed! Try again later.</em>'; ?>
+
+    <form action="/upload.php" method="post" enctype="multipart/form-data">
+        Upload an image
+        <input type="file" name="input_image">
+        <input type="submit" name="submit" value="Upload">
+    </form>
+
+    <hr>
+
+    <?php
+        require_once 'vendor/autoload.php';
+
+        $dotenv = Dotenv\Dotenv::create(__DIR__);
+        $dotenv->load();
+
+        \Cloudinary::config(array(
+            'cloud_name' => getenv('CLOUDINARY_CLOUD_NAME'), 
+            'api_key' => getenv('CLOUDINARY_API_KEY'), 
+            'api_secret' => getenv('CLOUDINARY_API_SECRET'),
+        ));
+
+        $redis = new Predis\Client(getenv('REDIS_URL'));
+
+        $image_ids = $redis->lrange('image_ids', 0, -1);
+        foreach ($image_ids as $image_id) {
+            echo "<br>";
+            echo "<div>";
+            $image_objects = $redis->hgetall('image_objects_' . $image_id);
+            foreach ($image_objects as $name => $frequency) {
+                echo $name . ': ' . $frequency;
+            }
+            echo "</div>";
+
+            $image_url = $redis->get('image_url_' . $image_id);
+            echo "<a href=\"{$image_url}\" target=\"_blank\">";
+            echo cl_image_tag($image_url, array(
+                'height' => 200,
+                'crop' => 'scale',
+            ));
+            echo "</a>";
+            echo "<br>";
+        }
+    ?>
+</body>
+</html>
---
+++ viewer/upload.php
@@ -0,0 +1,48 @@
+<?php
+    require_once 'vendor/autoload.php';
+
+    $dotenv = Dotenv\Dotenv::create(__DIR__);
+    $dotenv->load();
+
+    \Cloudinary::config(array(
+        'cloud_name' => getenv('CLOUDINARY_CLOUD_NAME'), 
+        'api_key' => getenv('CLOUDINARY_API_KEY'), 
+        'api_secret' => getenv('CLOUDINARY_API_SECRET'),
+    ));
+
+    if (is_uploaded_file($_FILES['input_image']['tmp_name'])) {
+        // upload image to Cloudinary
+        $image = \Cloudinary\Uploader::upload($_FILES['input_image']['tmp_name'], array(
+            'folder' => 'detector_svc_images/inputs',
+        ));
+
+        // make post request to detector service
+        $url = getenv('DETECTOR_SVC_URL');    
+        $content = json_encode(array('image_url' => $image['url']));
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+        $json_response = curl_exec($curl);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if ($status != 200) {
+            header('Location: /?processed=0');
+            exit();
+        }
+        curl_close($curl);
+        $response = json_decode($json_response, true);
+
+        // store response in redis
+        $redis = new Predis\Client(getenv('REDIS_URL'));
+        $image_id = uniqid();
+        $redis->lpush("image_ids", $image_id);
+        $redis->set('image_url_' . $image_id, $response['url']);
+        $redis->hmset('image_objects_' . $image_id, $response['objects']);
+
+        header('Location: /?new_output=1');
+    } else {
+        header('Location: /?input_image=0'); 
+    }
+    exit();
